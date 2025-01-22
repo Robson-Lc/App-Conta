7dde75182bef28bd085672affea7a4c908520cb5
Robson Luís <rblchagas@gmail.com>
2025-01-22 07:34:46 -0300

    Adicionado enntrada de dados

--- robson/bank/App.java
+++ robson/bank/App.java
@@ -17,26 +17,28 @@ public class App {
 
     Conta conta = new Conta("0011 ", "1234", "Nu Internacional");
 
-        boolean sucesso = conta.Sacar(200.0);
-    if(!sucesso){
-        System.out.println("Voçê não tem saldo suficiente para sacar");
+    while(true){
+        System.out.println("Escolha uma opção:  D= Depósito, S= saque, E= sair da conta");
+        String operacao = scanner.nextLine();
+
+        if(operacao.equals("D") || operacao.equals("d")){
+            System.out.println("Qual o valor do depósito?");
+            double valor = scanner.nextDouble();
+            conta.Depositar(valor);
+        }else if (operacao.equals("S") || operacao.equals("s")){
+            System.out.println("Qual o valor do saque?");
+            double valor = scanner.nextDouble();
+            if(!conta.Sacar(valor)){
+                System.out.println("Saldo insuficiente! Seu saldo é R$" + valor);
+            }
+        }else if(operacao.equals("E") || operacao.equals("e")){
+            System.out.println("Finalizando operação");
+            break;
+        }else{
+            System.out.println("Comando inválido, tente novamente");
+        }
+            scanner = new Scanner(System.in);
     }
-
-        conta.Depositar(100.0);
-        conta.Depositar(100.0);
-        conta.Depositar(100.0);
-
-
-        if (!conta.Sacar(200.0)){
-            System.out.println("Voçê não tem saldo suficiente para sacar");
-        }
-
-
-        if(!conta.Sacar(200.0)){
-            System.out.println("Voçê não tem saldo suficiente para sacar");
-
-        }
-        System.out.println(conta);
 
 
     }
